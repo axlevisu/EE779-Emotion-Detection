fba7bb30854af0365f3e49d4bbce00125baaed18
axle <axlevisu@gmail.com>
2016-10-06 12:24:01 +0530

    working EMD code

--- EMD.m
+++ EMD.m
@@ -1,15 +1,15 @@
 function IMF = EMD(x)
     c   = x(:)';
 %   SD predetermined value
-    pre_det = 0.3;
+    pre_det = 0.1;
     IMF = [];
     M=length(x);
-    while  1
+    while 1
         h = c;
         sd =1;
         %         Stops when i finds the residue
         if length(findpeaks(h))*length(findpeaks(-h)) ==0, break;end
-        while ~is_IMF(x) || sd > pre_det 
+        while sd > pre_det 
             max_index = find_peaks(h);
             min_index = find_peaks(-h);
             Upper_Envelope=spline(max_index, h(max_index), 1:M);
@@ -18,6 +18,7 @@ function IMF = EMD(x)
             m=(Upper_Envelope+Lower_Envelope)/2;
             eps = 0.0000001;
             sd = sum(m.^2)/(eps +sum(h.^2));
+%             display(sd)
             h=h-m;
         end
 
--- MainScript.m
+++ MainScript.m
@@ -1,1 +1,12 @@
 % Emotion Extration Main
+[x,Fs] = audioread('./male_pani_a_8k.wav');
+x=x';
+M=length(x);
+% Plot x
+% t=linspace(0,(M-1)/Fs, M);
+% figure(1); subplot(1,1,1);
+% plot(t, x, 'r');
+% axis tight;
+% title('Acoustic Speech Signal');
+
+imf = EMD(x);
